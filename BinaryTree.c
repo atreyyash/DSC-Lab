d27498bf07c6c36262ae40151e1e38c8aae8d3ad
Yash Atrey <atreyyash@gmail.com>
2022-12-05 15:00:37 +0530

    BT

---
+++ BinaryTree.c
@@ -0,0 +1,91 @@
+// Create a binary tree and find the traversal using recursion
+#include <stdio.h>
+#include <stdlib.h>
+
+// ONLY FOR VS CODE OR TERMINAL USERS :- input.txt file is used to give input so that we do not have to type again again
+
+
+// Sample Input : 8 10 1 -1 -1 6 4 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *createNode(int d)
+{
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = d;
+    new_node->left = NULL;
+    new_node->right = NULL;
+};
+
+struct node *createTree()
+{
+    int d;
+    scanf("%d", &d);
+    if (d == -1)
+    {
+        return NULL;
+    }
+    struct node *root = createNode(d);
+    root->left = createTree();
+    root->right = createTree();
+
+    return root;
+};
+
+void preorder(struct node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // recursive case
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+};
+
+void inorder(struct node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // recursive case
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+};
+
+void postorder(struct node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // recursive case
+    inorder(root->left);
+    inorder(root->right);
+    printf("%d ", root->data);
+};
+
+int main()
+{
+
+    struct node *root = createTree();
+    printf("\nPREORDER: \n");
+    preorder(root);
+    printf("\nINORDER:\n");
+    inorder(root);
+    printf("\nPOSTORDER: \n");
+    postorder(root);
+    return 0;
+}
